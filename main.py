90baf8e07db9f52bf3e5a4c26bf9615bbed95e28
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2024-10-11 16:22:32 +0300

    The closing windows code has been replaced to the true place in main.py file.

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
-        screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        screen.fill("black")
         pygame.display.flip()
 
     print("Starting asteroids!")
