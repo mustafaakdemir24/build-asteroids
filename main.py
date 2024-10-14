31fde94dc829656070e056605b0e7532719e1de2
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2024-10-14 13:57:49 +0300

    spaceship has been created

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -12,11 +13,14 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         
         # limit the framerate o 60 FPS
